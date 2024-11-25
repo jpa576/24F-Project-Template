66e32ed3bd36f67a9c01ca86c13623ef0334be57
AlejandroCampa <87829324+AlejandroCampa@users.noreply.github.com>
2024-11-25 13:19:01 -0500

    app

--- app/src/Home.py
+++ app/src/Home.py
@@ -8,18 +8,18 @@ import logging
 logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# import the main streamlit library as well
-# as SideBarLinks function from src/modules folder
+# Import the main Streamlit library as well as
+# SideBarLinks function from src/modules folder
 import streamlit as st
 from modules.nav import SideBarLinks
 
-# streamlit supports reguarl and wide layout (how the controls
+# Streamlit supports regular and wide layout (how the controls
 # are organized/displayed on the screen).
-st.set_page_config(layout = 'wide')
+st.set_page_config(layout='wide')
 
 # If a user is at this page, we assume they are not 
-# authenticated.  So we change the 'authenticated' value
-# in the streamlit session_state to false. 
+# authenticated. So we change the 'authenticated' value
+# in the Streamlit session_state to False. 
 st.session_state['authenticated'] = False
 
 # Use the SideBarLinks function from src/modules/nav.py to control
@@ -32,54 +32,48 @@ SideBarLinks(show_home=True)
 #    The major content of this page
 # ***************************************************
 
-# set the title of the page and provide a simple prompt. 
+# Set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('CS 3200 Sample Semester Project App: Algonauts edition')
+st.title('CS 3200 Sample Semester Project App: Algonauts Edition')
 st.write('\n\n')
-st.write('### Howdy! As which user would you like to log in?')
+st.write('### Welcome! Please select a user persona to log in:')
 
-# For each of the user personas for which we are implementing
-# functionality, we put a button on the screen that the user 
-# can click to MIMIC logging in as that mock user.
+# Create buttons for each persona and redirect to respective landing pages.
 
-
-if st.button("Act as John, a Political Strategy Advisor", 
-            type = 'primary', 
-            use_container_width=True):
-    # when user clicks the button, they are now considered authenticated
+if st.button("Persona 1: Ambitious Student - Jamie Chen", 
+             type='primary', 
+             use_container_width=True):
     st.session_state['authenticated'] = True
-    # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
-    # we add the first name of the user (so it can be displayed on 
-    # subsequent pages). 
-    st.session_state['first_name'] = 'John'
-    # finally, we ask streamlit to switch to another page, in this case, the 
-    # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+    st.session_state['role'] = 'ambitious_student'
+    st.session_state['first_name'] = 'Jamie'
+    logger.info("Logging in as Ambitious Student Persona")
+    st.switch_page('pages/01a_ambitious_student_home.py')
 
-if st.button('Act as Mohammad, an USAID worker', 
-            type = 'primary', 
-            use_container_width=True):
+if st.button("Persona 2: Career Shifter - Mark Davis", 
+             type='primary', 
+             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
+    st.session_state['role'] = 'career_shifter'
+    st.session_state['first_name'] = 'Mark'
+    logger.info("Logging in as Career Shifter Persona")
+    st.switch_page('pages/02_career_shifter_home.py')
 
-if st.button('Act as System Administrator', 
-            type = 'primary', 
-            use_container_width=True):
+if st.button("Persona 3: Job Seeker - Priya Patel", 
+             type='primary', 
+             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'administrator'
-    st.session_state['first_name'] = 'SysAdmin'
-    st.switch_page('pages/20_Admin_Home.py')
+    st.session_state['role'] = 'job_seeker'
+    st.session_state['first_name'] = 'Priya'
+    logger.info("Logging in as Job Seeker Persona")
+    st.switch_page('pages/03_job_seeker_home.py')
 
-#create the ui button for 1st user case
-if st.button('Algonauts user #1',
-            type = 'primary',
-            use_container_width=True):
+if st.button("Persona 4: Goal-Oriented User - Alex Reyes", 
+             type='primary', 
+             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = ''
-    st.session_state['first_name'] = 'Master'
-    st.switch_page('pages/01a_algonauts_user1_home.py')
+    st.session_state['role'] = 'goal_oriented_user'
+    st.session_state['first_name'] = 'Alex'
+    logger.info("Logging in as Goal-Oriented User Persona")
+    st.switch_page('pages/04_goal_oriented_user_home.py')
 
+# Add any additional personas or functionality here as necessary.
